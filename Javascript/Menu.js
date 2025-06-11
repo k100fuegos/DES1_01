acfbda60e2ed3fe2eee1a9db166ddea470ae0337
k100fuegos <kelvincienfuegos620@gmail.com>
2025-06-11 02:48:22 -0600

    vs 0.6

--- Javascript/Menu.js
+++ Javascript/Menu.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggle = document.getElementById("productosToggle");
   const submenu = document.getElementById("submenuProductos");
 
+  // Alternar el menú al hacer clic
   toggle.addEventListener("click", (e) => {
     e.preventDefault();
     submenu.classList.toggle("show");
   });
 
-  // Cerrar submenu al hacer click fuera
+  // Cerrar menú al hacer clic fuera de él
   document.addEventListener("click", (e) => {
     if (!toggle.contains(e.target) && !submenu.contains(e.target)) {
       submenu.classList.remove("show");
